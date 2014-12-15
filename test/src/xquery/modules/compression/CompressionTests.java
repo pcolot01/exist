1ff61baa303c51d2461d0a428490e56d7354153a
pcolot01 <pcolot@infeurope.lu>
2014-12-15 16:42:51 +0100

    Integration of xql based test suite in the build test process

---
+++ test/src/xquery/modules/compression/CompressionTests.java
@@ -0,0 +1,11 @@
+package xquery.modules.compression;
+
+import xquery.TestRunner;
+
+public class CompressionTests extends TestRunner {
+
+    @Override
+    protected String getDirectory() {
+        return "test/src/xquery/modules/compression";
+    }
+}
